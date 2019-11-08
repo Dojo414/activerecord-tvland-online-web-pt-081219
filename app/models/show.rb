7e4e17a20cd34155bd78eb9d770cdc2815a76c9c
Donovan Jones <DonovanLJones94@gmail.com>
2019-11-08 12:03:49 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
+require "pry"
 class Show < ActiveRecord::Base
   
+has_many :characters
+belongs_to :network
+
+def actors_list
+    self.characters.collect do |character|
+        "#{character.actor.first_name} #{character.actor.last_name}"
+    end
+end
+
 end
---
+++ db/migrate/006_add_catchphrase_column.rb
@@ -0,0 +1,8 @@
+class AddCatchphraseColumn < ActiveRecord::Migration[5.2]
+
+    def change
+        add_column :characters, :catchphrase, :string
+    end
+
+
+end
